c002353c3d3243149471f8b88d8fe2a0142c0273
LIU Yi <julien.liuy@gmail.com>
2021-02-13 15:14:47 +0800

    add redux

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
   'mongodb+srv://liuyi:liuyi123@cluster0.1sjle.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-const aa = [];
 mongoose
   .connect(CONNECTION_URL, {
     useNewUrlParser: true,

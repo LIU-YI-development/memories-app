7c488ccfa929510cd209dc8cbcbb007aff1980af
Yi LIU <julien.ly@gmail.com>
2021-05-29 20:30:32 +0200

    improve

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -36,3 +36,12 @@ export const updatePost = async (req, res) => {
   );
   res.json(updatedPost);
 };
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('No post with that id');
+  await PostMessage.findByIdAndRemove(id);
+  console.log('Delete!');
+  res.json({ message: 'Post delted successfully' });
+};
